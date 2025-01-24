ff904848b5d32143551a63b4f2970ab9e1e402d6
SIAG0N <phillipziwadah@gmail.com>
2025-01-24 13:58:39 +0000

    javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+// Display an alert message when the page loads
+window.alert("Welcome to my JavaScript project!");
+
+// Declare two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the two strings and display the result on the page
+document.write(Sent1 + Sent2);
+
+// Write an arithmetic expression to the console
+console.log("The result of 10 + 5 is: " + (10 + 5));
